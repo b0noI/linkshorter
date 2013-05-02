8aa73e78d7fd6645ae41a021df93a330315605b7
Viacheslav V. Kovalevskyi <Viacheslav@b0noI.com>
2013-05-02 14:56:20 +0300

    Closed issue #39

---
+++ src/test/java/ua/in/link/rest/BaseRestTest.java
@@ -0,0 +1,11 @@
+package ua.in.link.rest;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: b0noI
+ * Date: 02.05.13
+ * Time: 14:39
+ * To change this template use File | Settings | File Templates.
+ */
+public class BaseRestTest {
+}

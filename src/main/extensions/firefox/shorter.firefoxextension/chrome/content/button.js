fdde124a16b8327c23bb10c3110f9be0b82a25aa
Andrii Kryzhyk <mail2kandrew@gmail.com>
2014-03-11 23:46:16 +0000

    Basic form for displaying link

--- src/main/extensions/firefox/shorter.firefoxextension/chrome/content/button.js
+++ src/main/extensions/firefox/shorter.firefoxextension/chrome/content/button.js
@@ -1,3 +1,21 @@
+function handleOnClick() {
+    document.getElementById("lcoua-panel").openPopup;
+}
+
 function handleOnLoad() {
     var iframe = document.getElementById("lcoua-iframe");
+//    iframe.setAttribute("src","chrome://lcoua/content/iframe.html");
+    iframe.setAttribute("visible",true);
+    iframe.contentDocument.location.reload(true);
+//        var iframe = document.getElementById("lcoua-iframe");
+//        var doc = iframe.contentWindow.document;
+//    if (!doc.getElementById("lcoua-url")) {
+//        var input = doc.createElement("input");
+//        input.type = "text";
+//        input.size = 15;
+//        input.id = "lcoua-url";
+//        input.value = window.content.location.href;
+//        doc.body.appendChild(input);
+//    }
 }
+

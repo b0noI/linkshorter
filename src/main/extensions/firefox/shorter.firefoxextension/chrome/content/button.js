ea7be77bf8f617c5f38491257782ac35ab6dc5e0
Andrii Kryzhyk <mail2kandrew@gmail.com>
2014-03-13 09:19:07 +0000

    Fix of bug with iframe in panel

--- src/main/extensions/firefox/shorter.firefoxextension/chrome/content/button.js
+++ src/main/extensions/firefox/shorter.firefoxextension/chrome/content/button.js
@@ -1,21 +1,17 @@
 function handleOnClick() {
-    document.getElementById("lcoua-panel").openPopup;
 }
 
 function handleOnLoad() {
     var iframe = document.getElementById("lcoua-iframe");
-//    iframe.setAttribute("src","chrome://lcoua/content/iframe.html");
-    iframe.setAttribute("visible",true);
-    iframe.contentDocument.location.reload(true);
 //        var iframe = document.getElementById("lcoua-iframe");
-//        var doc = iframe.contentWindow.document;
-//    if (!doc.getElementById("lcoua-url")) {
-//        var input = doc.createElement("input");
+        var doc = iframe.contentWindow.document;
+        var input = doc.getElementById("lcoua-url");
+    if ( input ) {
 //        input.type = "text";
 //        input.size = 15;
 //        input.id = "lcoua-url";
-//        input.value = window.content.location.href;
+        input.value = window.content.location.href;
 //        doc.body.appendChild(input);
-//    }
+    }
 }
 
